1d60191074241efc622583480296da0e83440e3a
Alexey Strokin <alex.strok@gmail.com>
2018-11-13 17:49:59 +0300

    add padding for specific devices

--- DOAlertController/DOAlertController.swift
+++ DOAlertController/DOAlertController.swift
@@ -312,7 +312,14 @@ open class DOAlertController : UIViewController, UITextFieldDelegate, UIViewCont
         // variable for ActionSheet
         if (!isAlert()) {
             alertViewWidth =  screenSize.width
-            alertViewPadding = 8.0
+            let padding: CGFloat
+            switch UIDevice.current.kind {
+            case .iPad, .iPhone_5_5S_5C, .iPhone_6_6S_7_8, .iPhone_6_6S_7_8_PLUS, .iPhone_unknown:
+                padding = 8.0
+            case .iPhone_X_Xs, .iPhone_Xr, .iPhone_Xs_Max:
+                padding = 24.0
+            }
+            alertViewPadding = padding
             innerContentWidth = (screenSize.height > screenSize.width) ? screenSize.width - alertViewPadding * 2 : screenSize.height - alertViewPadding * 2
             buttonMargin = 8.0
             buttonCornerRadius = 6.0
@@ -768,5 +775,42 @@ open class DOAlertController : UIViewController, UITextFieldDelegate, UIViewCont
     
     open func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return DOAlertAnimation(isPresenting: false)
+    }
+}
+
+
+fileprivate extension UIDevice {
+    
+    fileprivate enum Kind {
+        case iPad
+        case iPhone_unknown
+        case iPhone_5_5S_5C
+        case iPhone_6_6S_7_8
+        case iPhone_6_6S_7_8_PLUS
+        case iPhone_X_Xs
+        case iPhone_Xs_Max
+        case iPhone_Xr
+    }
+    
+    fileprivate var kind: Kind {
+        if userInterfaceIdiom == .phone {
+            switch UIScreen.main.nativeBounds.height {
+            case 1136:
+                return .iPhone_5_5S_5C
+            case 1334:
+                return .iPhone_6_6S_7_8
+            case 1920, 2208:
+                return .iPhone_6_6S_7_8_PLUS
+            case 2436:
+                return .iPhone_X_Xs
+            case 2688:
+                return .iPhone_Xs_Max
+            case 1792:
+                return .iPhone_Xr
+            default:
+                return .iPhone_unknown
+            }
+        }
+        return .iPad
     }
 }
